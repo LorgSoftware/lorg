d1edd82260526307f32625d25802b4932c83f1d4
nales <>
2022-10-05 10:37:44 +0000

    Take a file as an argument and check if it exists
    FossilOrigin-Name: 8abe5d1348092817c4bff164257be7f43983585d3d6f8d23388ee6229cc2e982

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <filesystem>
+
+constexpr int EXIT_CODE_OK = 0;
+constexpr int EXIT_CODE_ERROR_ARGUMENTS = 1;
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    // Check the argument count
+    if(argc < 2)
+    {
+        std::cerr << "Need a file as an argument" << std::endl;
+        exit(EXIT_CODE_ERROR_ARGUMENTS);
+    }
 
-    return 0;
+    // Check if file exists
+    auto filepath = argv[1];
+    {
+        std::filesystem::path f(filepath);
+        if(!std::filesystem::exists(f))
+        {
+            std::cerr << "The file \"" << filepath << "\" does not exist." << std::endl;
+            exit(EXIT_CODE_ERROR_ARGUMENTS);
+        }
+    }
+
+    return EXIT_CODE_OK;
 }
