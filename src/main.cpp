9d29825b0397e75dc140d7d50693861de6204d8c
nales <>
2022-10-05 10:57:21 +0000

    Make the comments syntactically correct sentences
    FossilOrigin-Name: 86cadc0e04bed79467510435c603dc21f22f2e7e65316660eb3a963bf62df4a5

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@ constexpr int EXIT_CODE_ERROR_ARGUMENTS = 1;
 
 int main(int argc, char* argv[])
 {
-    // Check the argument count
+    // Check the argument count.
     if(argc < 2)
     {
         std::cerr << "Need a file as an argument" << std::endl;
         exit(EXIT_CODE_ERROR_ARGUMENTS);
     }
 
-    // Check if file exists
+    // Check if file exists.
     auto filepath = argv[1];
     {
         std::filesystem::path f(filepath);
